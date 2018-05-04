e4f6e59bf5c32274499a3919c2885fe73447e6f1
alpha-dever <alphadever@gmail.com>
2018-05-04 16:28:31 -0400

    added file system read/write

--- notes-node/notes.js
+++ notes-node/notes.js
@@ -1,8 +1,29 @@
 /*jshint esversion: 6 */
 console.log('starting notes.js ...');
+const fs = require('fs');
 
 var addNote = (title, body) => {
-    console.log('Adding note', title, body);
+    var notes = [];
+    var note = {
+        title,
+        body
+    };
+
+    try {
+        notes = JSON.parse(fs.readFileSync('notes-data.json'));
+    } catch (error) {
+
+    }
+
+    var duplicateNotes = notes.filter((note) => {
+        return note.title === title;
+    });
+
+    if(duplicateNotes.length ===0)
+    {
+        notes.push(note);
+        fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    }
 };
 
 var getAll = () => {
